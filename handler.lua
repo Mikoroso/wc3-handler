0a3921181b42f777871563d128f90a4b7f6eeccd
Mikoroso <mikorosolain@gmail.com>
2020-06-01 18:13:14 -0700

    Add Groups
    Adds Handler for Groups too.

--- handler.lua
+++ handler.lua
@@ -10,9 +10,20 @@ Handles = {
         Timer = {}
     }
 
+
+
 }
 
 function msg(text) DisplayTimedTextToForce(GetPlayersAll(), 5., text) end
+
+function CreateObject(text) 
+    local func = "Create" .. text
+    return func()
+end
+
+CreateObject(Group)
+
+
 
 -- **Creates or use paused timer from Wseless.Timer table
 -- @returns | timer
@@ -56,6 +67,67 @@ function CleanTimer(timer)
                 table.insert(Handles.Useless.Timer, uselessTimer)
             else 
                 DestroyTimer(timer) 
+            end
+
+            break
+
+        end
+    end
+end
+
+
+
+-- **Creates or use cleaned group from Wseless.Timer table
+-- @returns | group
+function NewGroup(objtype)
+    --local useless = Handles.Useless
+    --local useful = Handles.Useful
+
+    --useless = useless[objtype]
+    --useful = useful[objtype]
+
+
+
+
+    local useless = Handles.Useless.Group
+    local object
+
+    if #useless > 0 then
+        -- msg("Use past timer")
+        object = useless[#useless]
+        useless[#useless] = nil
+    else
+        -- msg("Create New Timer")
+        object = CreateGroup()
+    end
+
+    table.insert(Handles.Useful.Group,object)
+
+    return object
+end
+
+-- **Clean and Replace group to Useless
+-- @params | target group
+function CleanGroup(object)
+    local useful = Handles.Useful.Group
+
+    CleanGroup(object)
+
+    for i = 1, #useful do
+
+        if object == useful[i] then
+
+            local last = useful[#useful]
+            local uselessObject = useful[i]
+
+            -- msg("Timer with index :" .. i .. " was removed")
+            useful[i] = last
+            useful[#useful] = nil
+
+            if #Handles.Useless.Group <= Handles.MaxCounter then
+                table.insert(Handles.Useless.Group, uselessObject)
+            else 
+                DestroyGroup(object) 
             end
 
             break
